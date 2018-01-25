4ff6be7b52a314eec9541be459d2bdb1d43f3241
Pawel Kwiecien <pawel.b.kwiecien@gmail.com>
2018-01-25 11:20:11 +0100

    Creating and populating board

--- src/pl/pawelkwiecien/battleship/BoardManager.java
+++ src/pl/pawelkwiecien/battleship/BoardManager.java
@@ -2,6 +2,14 @@ package pl.pawelkwiecien.battleship;
 
 public class BoardManager {
 
-    //TODO implement
+    static BoardCell[][] createBoard() {
+        BoardCell[][] gameBoard = new BoardCell[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                gameBoard[i][j] = new BoardCell();
+            }
+        }
+        return gameBoard;
+    }
 
 }
